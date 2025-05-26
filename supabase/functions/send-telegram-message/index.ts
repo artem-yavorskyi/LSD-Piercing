409a10f078dc95575852b4f55d3a691f593b788c
Artem Yavorskyi <uayoungin@gmail.com>
2025-05-26 23:27:18 +0300

    fix: add cors headers to edge func

--- supabase/functions/send-telegram-message/index.ts
+++ supabase/functions/send-telegram-message/index.ts
@@ -3,19 +3,21 @@ import {serve} from "https://deno.land/std/http/server.ts";
 
 
 Deno.serve(async (req) => {
-if (req.method === "OPTIONS") {
-  return new Response("ok", {
-    headers: {
-    "Access-Control-Allow-Origin": "*",
+ const corsHeaders = {
+    "Access-Control-Allow-Origin": "*", 
     "Access-Control-Allow-Methods": "POST, OPTIONS",
-     "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    },
-    status: 200
-  })
-}
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  }
+
+  if (req.method === "OPTIONS") {
+    return new Response("ok", {
+      headers: corsHeaders,
+      status: 200
+    })
+  }
 
   if (req.method !== 'POST') {
-    return new Response("Method Not Allowed", {status: 405, headers: {"Allow": "POST"}})
+    return new Response("Method Not Allowed", {status: 405, headers: {...corsHeaders, "Allow": "POST"}})
   }
 
   let requestData = await req.json()
@@ -26,7 +28,10 @@ if (req.method === "OPTIONS") {
   const chatId = Deno.env.get("TELEGRAM_CHAT_ID")
 
   if (!token || !chatId) {
-    return new Response("Telegraam secrets not set", {status: 500})
+    return new Response(
+      "Telegraam secrets not set", 
+      {status: 500, 
+      headers: corsHeaders})
   }
 
   let message = `Новий запис:\n\nІм'я: ${name}\nПрізвище: ${last_name}\nТелефон: ${phone_number}\nДата: ${selected_date}\nЧас: ${selected_time}\nІнстарам: ${instagram}\nКоментар: ${comment}`
@@ -34,19 +39,19 @@ if (req.method === "OPTIONS") {
   try {
     const telegramResponse = await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
     method: "POST",
-    headers: {"Content-Type": "application/json"},
+    headers: {"Content-Type": "application/json", ...corsHeaders},
     body: JSON.stringify({chat_id: chatId, text:message})
     })
   
     if (!telegramResponse.ok) {
       const errorText = await telegramResponse.text()
       console.log("Telegram API Error:", errorText)
-      return new Response(`Telegram Error: ${errorText}, {status: 500}`)
+      return new Response(`Telegram Error: ${errorText}`, {status: 500, headers: corsHeaders})
     }
 
-    return new Response("Sent message", {status: 200})
+    return new Response("Sent message", {status: 200, headers: corsHeaders})
 
   } catch(error) {
-    return new Response(`Server errror: ${error.message}`, {status: 500})
+    return new Response(`Server errror: ${error.message}`, {status: 500, headers: corsHeaders})
     }
 })
