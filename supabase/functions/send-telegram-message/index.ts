0d5ecfc1eed1a093d9cf5e80102439f49bcd252f
Artem <uayoungin@gmail.com>
2025-05-27 15:46:37 +0300

    edit: telegram message

--- supabase/functions/send-telegram-message/index.ts
+++ supabase/functions/send-telegram-message/index.ts
@@ -1,57 +1,82 @@
-import "jsr:@supabase/functions-js/edge-runtime.d.ts"
-import {serve} from "https://deno.land/std/http/server.ts";
-
+// deno-lint-ignore-file
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 
 Deno.serve(async (req) => {
- const corsHeaders = {
-    "Access-Control-Allow-Origin": "*", 
+  const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "POST, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
-  }
+  };
 
   if (req.method === "OPTIONS") {
     return new Response("ok", {
       headers: corsHeaders,
-      status: 200
-    })
+      status: 200,
+    });
   }
 
-  if (req.method !== 'POST') {
-    return new Response("Method Not Allowed", {status: 405, headers: {...corsHeaders, "Allow": "POST"}})
+  if (req.method !== "POST") {
+    return new Response("Method Not Allowed", {
+      status: 405,
+      headers: { ...corsHeaders, Allow: "POST" },
+    });
   }
 
-  let requestData = await req.json()
+  let requestData = await req.json();
 
-  const {name, last_name, phone_number, instagram, comment, selected_date, selected_time} = requestData
+  const {
+    name,
+    last_name,
+    phone_number,
+    instagram,
+    comment,
+    selected_date,
+    selected_time,
+  } = requestData;
 
-  const token = Deno.env.get("TELEGRAM_BOT_TOKEN")
-  const chatId = Deno.env.get("TELEGRAM_CHAT_ID")
+  const token = Deno.env.get("TELEGRAM_BOT_TOKEN");
+  const chatId = Deno.env.get("TELEGRAM_CHAT_ID");
 
   if (!token || !chatId) {
-    return new Response(
-      "Telegraam secrets not set", 
-      {status: 500, 
-      headers: corsHeaders})
+    return new Response("Telegraam secrets not set", {
+      status: 500,
+      headers: corsHeaders,
+    });
   }
 
-  let message = `Новий запис:\n\nІм'я: ${name}\nПрізвище: ${last_name}\nТелефон: ${phone_number}\nДата: ${selected_date}\nЧас: ${selected_time}\nІнстарам: ${instagram}\nКоментар: ${comment}`
+  let message = `🔔 Новий запис:\n\n👤 | Ім'я, прізвище: ${name} ${last_name}\n📱 | Телефон: ${phone_number}\n📅 | Дата: ${selected_date}\n⏳ | Час: ${selected_time}`;
+
+  if (instagram) {
+    message += `\n📷 | Інстарам: ${instagram}`;
+  }
+  if (comment) {
+    message += `\n💬 | Коментар: ${comment}`;
+  }
 
   try {
-    const telegramResponse = await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
-    method: "POST",
-    headers: {"Content-Type": "application/json", ...corsHeaders},
-    body: JSON.stringify({chat_id: chatId, text:message})
-    })
-  
+    const telegramResponse = await fetch(
+      `https://api.telegram.org/bot${token}/sendMessage`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json", ...corsHeaders },
+        body: JSON.stringify({ chat_id: chatId, text: message }),
+      }
+    );
+
     if (!telegramResponse.ok) {
-      const errorText = await telegramResponse.text()
-      console.log("Telegram API Error:", errorText)
-      return new Response(`Telegram Error: ${errorText}`, {status: 500, headers: corsHeaders})
+      const errorText = await telegramResponse.text();
+      console.log("Telegram API Error:", errorText);
+      return new Response(`Telegram Error: ${errorText}`, {
+        status: 500,
+        headers: corsHeaders,
+      });
     }
 
-    return new Response("Sent message", {status: 200, headers: corsHeaders})
-
-  } catch(error) {
-    return new Response(`Server errror: ${error.message}`, {status: 500, headers: corsHeaders})
-    }
-})
+    return new Response("Sent message", { status: 200, headers: corsHeaders });
+  } catch (error) {
+    return new Response(`Server error: ${(error as Error).message}`, {
+      status: 500,
+      headers: corsHeaders,
+    });
+  }
+});
