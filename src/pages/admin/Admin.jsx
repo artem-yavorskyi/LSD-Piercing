05a0781e7c7e2aa50896d6b01d279ba639a9f884
Artem Yavorskyi <uayoungin@gmail.com>
2025-06-15 14:47:39 +0300

    fix: vercel.json

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -8,7 +8,7 @@ const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-const GET_ALL_BOOKINGS_PROXY_PATH = "/supabase/functions/get-all-bookings";
+const GET_ALL_BOOKINGS_PROXY_PATH = "/api/get-all-bookings";
 
 import "../../styles/admin/admin.css";
 
